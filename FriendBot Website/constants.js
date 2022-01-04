2b73b5efde02c0b75b3f35b51fb011e6689aa603
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2022-01-04 15:36:53 +0700

    Create constants.js

---
+++ FriendBot Website/constants.js
@@ -0,0 +1,81 @@
+// possible inputs
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself",
+    "what are you"
+  ],
+  ["i love you", "do you love me"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today", "what is your favourite food", "what is your favourite dish"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""], 
+  ["haha","ha","lol","hehe","funny","joke"],
+  ["What do you think of Philipus Tandra Adriel"]
+]
+
+// possible respons according to input
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am as old as time itself, been here since the beginning and I'll be here in the end."],
+  ["I am just a bot with feelings", "I am a bot with feelings. What are you?"],
+  ["My creator is Peter Nelson Subrata, he is very cool and sexy"],
+  ["My name is FriendBot, Nice to meet you", "You don't need to know my name, just know that we are friends =)"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza", "Curry"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"],
+  ["I think he is sucks"]
+]
+
+// shallow outputs
+
+const alternative = [
+  "Same",
+  "Go on",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I understand =)"
+]
+
+// else
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times", "Stay safe"]
+const games = ["Im sort of a gamer myself", "I'm Global Elite in CSGO, no aimbot promise =)"]
+const name = ["Hey there Champ, My name is FriendBot", "Nice name it perfectly fits who you are", "My name is FriendBot, Nice to meet you"]
